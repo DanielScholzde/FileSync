91a6a25574625d447138093e87b124d25a2c8f4c
Daniel Scholz <danielsch@gmx.net>
2024-06-11 21:13:42 +0200

    init

--- src/main/java/de/danielscholz/fileSync/matching/EqualsByIChangeTo.kt
+++ src/main/java/de/danielscholz/fileSync/matching/EqualsByIChangeTo.kt
@@ -1,24 +1,20 @@
 package de.danielscholz.fileSync.matching
 
-import de.danielscholz.fileSync.actions.sync.IChange
-import de.danielscholz.fileSync.persistence.File2
-import java.util.*
 
-
-fun <R> equalsByTo(mode: EnumSet<MatchMode>, ignoreDuplicatesOnIntersect: Boolean = false, block: EqualsBy<IChange<*, File2>>.() -> R): R {
-
-    val equalsBy = EqualsBy(ignoreDuplicatesOnIntersect, object : EqualsAndHashCodeSupplier<IChange<*, File2>> {
-
-        val eq = EqualsAndHashCodeSupplierImpl(mode)
-
-        override fun equals(obj1: IChange<*, File2>, obj2: IChange<*, File2>): Boolean {
-            return eq.equals(obj1.to, obj2.to)
-        }
-
-        override fun hashCode(obj: IChange<*, File2>): Int {
-            return eq.hashCode(obj.to)
-        }
-    })
-
-    return equalsBy.block()
-}
+//fun <R> equalsByTo(mode: EnumSet<MatchMode>, ignoreDuplicatesOnIntersect: Boolean = false, block: EqualsBy<IChange<*, File2>>.() -> R): R {
+//
+//    val equalsBy = EqualsBy(ignoreDuplicatesOnIntersect, object : EqualsAndHashCodeSupplier<IChange<*, File2>> {
+//
+//        val eq = EqualsAndHashCodeSupplierImpl(mode)
+//
+//        override fun equals(obj1: IChange<*, File2>, obj2: IChange<*, File2>): Boolean {
+//            return eq.equals(obj1.to, obj2.to)
+//        }
+//
+//        override fun hashCode(obj: IChange<*, File2>): Int {
+//            return eq.hashCode(obj.to)
+//        }
+//    })
+//
+//    return equalsBy.block()
+//}
