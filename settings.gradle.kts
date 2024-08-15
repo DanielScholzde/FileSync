3aa033a58153b0588c14066ee5ba9a9e4aac5638
Daniel Scholz <danielsch@gmx.net>
2024-08-15 12:05:02 +0200

    Amper update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,9 @@ pluginManagement {
 // config for Amper:
 //pluginManagement {
 //    repositories {
+//        mavenCentral()
 //        google()
+//        gradlePluginPortal()
 //        maven("https://packages.jetbrains.team/maven/p/amper/amper")
 //        maven("https://www.jetbrains.com/intellij-repository/releases")
 //        maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
